7dc200d1e77a5293459cdd1fcf65e6a574413d2a
Anthony Jaimes <ajaimesflower@gmail.com>
2021-05-14 01:25:32 -0700

    added tools on website

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -2,24 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Tool_Website.data;
+using Tool_Website.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 namespace Tool_Website.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly toolcontext db;  
+        public IndexModel(toolcontext db) => this.db = db;
+        public List<product> Products { get; set; } = new List<product>();  
+        public product FeaturedProduct { get; set; }  
+        public async Task OnGetAsync()
         {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-
+            Products = await db.Products.ToListAsync();
+            FeaturedProduct = Products.ElementAt(new Random().Next(Products.Count));
         }
     }
 }
